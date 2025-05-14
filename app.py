706612c04f9d03c156df751d348ff76d159cf9cb
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2025-05-14 09:46:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
 load_dotenv()
 
 app = Flask(__name__)
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
